68ea3d193b9282e9d6b695091e508d3be33d4d61
prajaktaranjane80-sketch <prajaktaranjane80@gmail.com>
2026-09-12 13:44:46 +0530

    Create test_reconciliation_policy.py

---
+++ REOS_CONTROL_CENTER/AUTONOMY_ENGINE/continuity/acrl/T28_Conflict_Resolution_Reconciliation_Engine/test_reconciliation_policy.py
@@ -0,0 +1,13 @@
+import pytest
+
+from reconciliation_models import ReconciliationPolicy
+from reconciliation_policy import validate_policy
+
+
+def test_policy_valid():
+    validate_policy(ReconciliationPolicy())
+
+
+def test_policy_requires_version():
+    with pytest.raises(ValueError):
+        validate_policy(ReconciliationPolicy(policy_version=""))
