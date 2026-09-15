0aea9e975078eee18cc7ca3354eb7ed6d4e0e458
prajaktaranjane80-sketch <prajaktaranjane80@gmail.com>
2026-09-15 16:44:20 +0530

    Update __init__.py

--- REOS_CONTROL_CENTER/AUTONOMY_ENGINE/continuity/acrl/__init__.py
+++ REOS_CONTROL_CENTER/AUTONOMY_ENGINE/continuity/acrl/__init__.py
@@ -9,6 +9,20 @@ from .project_dna import (
     read_project_dna,
 )
 
+from .repository_context import (
+    AUTHORITY,
+    SCHEMA_VERSION,
+    RepositoryContextDecision,
+    RepositoryContextReason,
+    RepositoryContextError,
+    RepositoryContextAuthorityError,
+    RepositoryContextValidationError,
+    RepositoryContextRequest,
+    RepositoryContextReport,
+    RepositoryContextEngine,
+    resolve_repository_context,
+)
+
 __all__ = [
     "ProjectDNA",
     "ProjectDNAError",
@@ -16,4 +30,15 @@ __all__ = [
     "ProjectDNASourceError",
     "ProjectDNAReader",
     "read_project_dna",
+    "AUTHORITY",
+    "SCHEMA_VERSION",
+    "RepositoryContextDecision",
+    "RepositoryContextReason",
+    "RepositoryContextError",
+    "RepositoryContextAuthorityError",
+    "RepositoryContextValidationError",
+    "RepositoryContextRequest",
+    "RepositoryContextReport",
+    "RepositoryContextEngine",
+    "resolve_repository_context",
 ]
