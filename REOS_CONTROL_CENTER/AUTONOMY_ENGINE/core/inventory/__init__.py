df4f68af3e0785e1c1af6242141b26165d0d185c
prajaktaranjane80-sketch <prajaktaranjane80@gmail.com>
2026-09-25 18:17:14 +0530

    Update __init__.py

--- REOS_CONTROL_CENTER/AUTONOMY_ENGINE/core/inventory/__init__.py
+++ REOS_CONTROL_CENTER/AUTONOMY_ENGINE/core/inventory/__init__.py
@@ -1,1 +1,19 @@
-﻿# REOS bounded layer: inventory
+"""REOS CORE-004 Inventory bounded domain."""
+
+from .inventory import (
+    Inventory,
+    InventoryDomainError,
+    InventoryEvent,
+    InventoryProjectViolation,
+    InventoryTenantViolation,
+    InventoryType,
+)
+
+__all__ = [
+    "Inventory",
+    "InventoryDomainError",
+    "InventoryEvent",
+    "InventoryProjectViolation",
+    "InventoryTenantViolation",
+    "InventoryType",
+]
