609be51bc187456e73daff50f61ce2a964424b8a
prajaktaranjane80-sketch <prajaktaranjane80@gmail.com>
2026-09-26 21:10:54 +0530

    Update __init__.py

--- REOS_CONTROL_CENTER/AUTONOMY_ENGINE/core/event_platform/__init__.py
+++ REOS_CONTROL_CENTER/AUTONOMY_ENGINE/core/event_platform/__init__.py
@@ -1,1 +1,34 @@
-﻿# REOS bounded layer: event_platform
+from .event_domain import (
+    EventDomainError,
+    EventValidationError,
+    EventTenantViolation,
+    EventMutationError,
+    EventSerializationError,
+    EventTraceContext,
+    EventEnvelope,
+)
+
+from .event_contract import (
+    EventContractError,
+    UnknownEventTypeError,
+    UnsupportedEventVersionError,
+    IncompatibleEventSchemaError,
+    EventContract,
+    EventContractRegistry,
+)
+
+__all__ = [
+    "EventDomainError",
+    "EventValidationError",
+    "EventTenantViolation",
+    "EventMutationError",
+    "EventSerializationError",
+    "EventTraceContext",
+    "EventEnvelope",
+    "EventContractError",
+    "UnknownEventTypeError",
+    "UnsupportedEventVersionError",
+    "IncompatibleEventSchemaError",
+    "EventContract",
+    "EventContractRegistry",
+]
