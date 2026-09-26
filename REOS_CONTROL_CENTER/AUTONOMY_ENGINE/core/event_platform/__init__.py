a35f9f90fdf8c16278302e5e270fa866a5bc0748
prajaktaranjane80-sketch <prajaktaranjane80@gmail.com>
2026-09-26 21:27:18 +0530

    Update __init__.py

--- REOS_CONTROL_CENTER/AUTONOMY_ENGINE/core/event_platform/__init__.py
+++ REOS_CONTROL_CENTER/AUTONOMY_ENGINE/core/event_platform/__init__.py
@@ -1,3 +1,9 @@
+"""
+CORE-002 Event & Platform Core.
+
+Transport-independent event domain and integration boundaries.
+"""
+
 from .event_domain import (
     EventDomainError,
     EventValidationError,
@@ -17,6 +23,106 @@ from .event_contract import (
     EventContractRegistry,
 )
 
+from .event_delivery import (
+    DeliveryError,
+    DeliveryAuthorizationError,
+    DuplicateDeliveryError,
+    DeliveryStateError,
+    DeliveryStatus,
+    DeliveryFailureClass,
+    DeliveryAttempt,
+    DeliveryRecord,
+    DeliveryPolicy,
+    EventDeliveryCoordinator,
+)
+
+from .event_transaction import (
+    TransactionBoundaryError,
+    InvalidPublicationStateError,
+    PublicationState,
+    OutboxContract,
+    PublicationStateMachine,
+    build_publication_contract,
+)
+
+from .event_replay import (
+    ReplayError,
+    ReplayAuthorizationError,
+    ReplayScopeError,
+    ReplayDuplicateError,
+    ReplayStatus,
+    ReplayScope,
+    ReplayRequest,
+    ReplayResult,
+    ReplayAuthorizationBoundary,
+    ReplayCoordinator,
+)
+
+from .event_ordering import (
+    OrderingError,
+    StaleEventError,
+    OutOfOrderEventError,
+    OrderingViolation,
+    StreamPosition,
+    OrderingDecision,
+    EventOrderingGuard,
+    validate_stream_identity,
+)
+
+from .event_resilience import (
+    ResilienceError,
+    RetryExhaustedError,
+    PoisonMessageError,
+    FailureClass,
+    FailureDisposition,
+    FailureClassification,
+    RetryDecision,
+    QuarantineRecord,
+    FailureClassifier,
+    RetryPolicy,
+    ResilienceBoundary,
+)
+
+from .event_observability import (
+    EvidenceType,
+    EventEvidence,
+    DeliveryMetric,
+    EventEvidenceFactory,
+    validate_evidence_tenant,
+)
+
+from .event_security import (
+    EventSecurityError,
+    CrossTenantEventError,
+    ProducerAuthorizationError,
+    ConsumerAuthorizationError,
+    EventIntegrityError,
+    ProducerIdentity,
+    ConsumerIdentity,
+    EventSecurityBoundary,
+)
+
+from .event_reos_contract import (
+    REOSEventContractError,
+    REOSIntegrationContract,
+    REOSIntegrationBoundary,
+)
+
+from .event_acrl_contract import (
+    ACRLEventContractError,
+    ACRLReconstructionDescriptor,
+    ACRLCheckpointDescriptor,
+    ACRLIntegrationBoundary,
+)
+
+from .event_transport import (
+    TransportAdapterError,
+    TransportSerializationError,
+    TransportMessage,
+    EventTransportAdapter,
+    TransportRegistry,
+)
+
 __all__ = [
     "EventDomainError",
     "EventValidationError",
@@ -31,4 +137,74 @@ __all__ = [
     "IncompatibleEventSchemaError",
     "EventContract",
     "EventContractRegistry",
+    "DeliveryError",
+    "DeliveryAuthorizationError",
+    "DuplicateDeliveryError",
+    "DeliveryStateError",
+    "DeliveryStatus",
+    "DeliveryFailureClass",
+    "DeliveryAttempt",
+    "DeliveryRecord",
+    "DeliveryPolicy",
+    "EventDeliveryCoordinator",
+    "TransactionBoundaryError",
+    "InvalidPublicationStateError",
+    "PublicationState",
+    "OutboxContract",
+    "PublicationStateMachine",
+    "build_publication_contract",
+    "ReplayError",
+    "ReplayAuthorizationError",
+    "ReplayScopeError",
+    "ReplayDuplicateError",
+    "ReplayStatus",
+    "ReplayScope",
+    "ReplayRequest",
+    "ReplayResult",
+    "ReplayAuthorizationBoundary",
+    "ReplayCoordinator",
+    "OrderingError",
+    "StaleEventError",
+    "OutOfOrderEventError",
+    "OrderingViolation",
+    "StreamPosition",
+    "OrderingDecision",
+    "EventOrderingGuard",
+    "validate_stream_identity",
+    "ResilienceError",
+    "RetryExhaustedError",
+    "PoisonMessageError",
+    "FailureClass",
+    "FailureDisposition",
+    "FailureClassification",
+    "RetryDecision",
+    "QuarantineRecord",
+    "FailureClassifier",
+    "RetryPolicy",
+    "ResilienceBoundary",
+    "EvidenceType",
+    "EventEvidence",
+    "DeliveryMetric",
+    "EventEvidenceFactory",
+    "validate_evidence_tenant",
+    "EventSecurityError",
+    "CrossTenantEventError",
+    "ProducerAuthorizationError",
+    "ConsumerAuthorizationError",
+    "EventIntegrityError",
+    "ProducerIdentity",
+    "ConsumerIdentity",
+    "EventSecurityBoundary",
+    "REOSEventContractError",
+    "REOSIntegrationContract",
+    "REOSIntegrationBoundary",
+    "ACRLEventContractError",
+    "ACRLReconstructionDescriptor",
+    "ACRLCheckpointDescriptor",
+    "ACRLIntegrationBoundary",
+    "TransportAdapterError",
+    "TransportSerializationError",
+    "TransportMessage",
+    "EventTransportAdapter",
+    "TransportRegistry",
 ]
